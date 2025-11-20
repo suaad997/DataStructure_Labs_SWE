4ab172218208c13852b8d9b388111014b15c23a9
suada <suadaljuaid1@gmail.com>
2025-11-20 13:29:44 +0300

    lab2- Arrays -p1

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -1,6 +1,9 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Arrayss {
+    Scanner scanner =new Scanner(System.in);
 
 
     public void Traversal(int []a){
@@ -8,7 +11,32 @@ public class Arrayss {
             System.out.println("Index["+i+"] = "+a[i]);
 
     }
+    public void Traversal2(int []a){
+        for (int e:a)
+            System.out.println(e);
 
+    }
+
+    public void R_Traversal(int []a){
+        for (int i=a.length-1; i>=0;i--)
+            System.out.println("Index["+i+"] = "+a[i]);
+
+    }
+
+
+    public void input(int[]a){
+        System.out.println("enter  "+a.length+ " numbers:");
+        for(int i=0; i<a.length;i++)
+            a[i]=scanner.nextInt();
+    }
+    public  void update(int []a, int index, int nvalue){
+        a[index]=nvalue;
+
+    }
+    public  void delete(int []a, int index){
+        a[index]=0;
+
+    }
 
 
 
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,10 +1,19 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Arrayss  arr= new Arrayss();
         int []numbers={1,2,3,4,5};
-        int []numbers2=new int[5];
+
+        numbers[3]=8;
+        //arr.update(numbers,3,8);
+        arr.delete(numbers,3);
+
+        //int []numbers2=new int[5];
+        //arr.input(numbers2);
+        // arr.R_Traversal(numbers);
         arr.Traversal(numbers);
 
 
