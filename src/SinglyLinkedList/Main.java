913d36d4e38d55206331659124bf6212bc0a6ec8
suada <suadaljuaid1@gmail.com>
2025-12-09 21:42:18 +0300

    lab5- SinglyLinkedList pt1

--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -1,4 +1,15 @@
 package SinglyLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.removeFirst();
+        list.display();
+
+    }
 }
--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -1,40 +1,102 @@
 package SinglyLinkedList;
 
 public class SinglyLinkedList<E> {
-    private Node<E> head=null;
-    private Node<E> tail=null;
-    private int size=0;
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
 
-public int size(){
-    return size;
-}
-public boolean isEmpty(){
-    return size()==0;
+    public int size() {
+        return size;
+    }
 
-}
-public void addFirst(E data){
-    Node<E> nNode= new Node(data,head);
-    head=nNode;
-    if(isEmpty())
-        tail=head;
+    public boolean isEmpty() {
+        return size() == 0;
 
-    size++;
-}
+    }
 
-public E getFirst(){
-    if(isEmpty())return null;
-    return head.getData();
-}
+    public void addFirst(E data) {
+        Node<E> nNode = new Node(data, head);
+        head = nNode;
+        if (isEmpty())
+            tail = head;
+        size++;
+    }
 
-public E removeFirst(){
-    if(isEmpty())return null;
-    E delete = head.getData();
-    head=head.getNext();
-    size--;
-    if (size()==0)
-        tail=head;
+    public E getFirst() {
+        if (isEmpty()) return null;
+        return head.getData();
+    }
 
-return delete;
+    public E removeFirst() {
+        if (isEmpty()) return null;
+        E delete = head.getData();
+        head = head.getNext();
+        size--;
+        if (size() == 0)
+            tail = head;
+        return delete;
+    }
+
+    public void addLast(E data) {
+        Node<E> nNode = new Node(data, null);
+        if (isEmpty()) {
+            head = nNode;
+        } else {
+            tail.setNext(nNode);
+        }
+        tail = nNode;
+        size++;
+    }
+
+    public E getLast() {
+        if (isEmpty()) return null;
+        return tail.getData();
+    }
+
+    public E removeLast() {
+        if (isEmpty()) return null;
+        E delete = tail.getData();
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            Node<E> temp = head;
+            while (temp.getNext() != tail) {
+                temp = temp.getNext();
+            }
+            temp.setNext(null);
+            tail = temp;
+        }
+        size--;
+        return delete;
+    }
+
+    public void display() {
+        Node<E> temp = head;
+        while (temp != null) {
+            System.out.print(temp.getData() + "----->");
+            temp = temp.getNext();
+
+        }
+        System.out.println("null");
+
+
+    }
+public void addAtPosition(E data,int position) {
+       if(position<=0 || position>size+1)
+           throw new IllegalArgumentException("Invaild position");
+
+       if (position==1){
+           addFirst(data);
+       return;}
+
+
+
+    }
+
+
+
+
 }
 
 
@@ -64,4 +126,4 @@ class Node<E>  {
     }
 }
 
-}
+
